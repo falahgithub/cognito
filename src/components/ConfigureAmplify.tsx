73f177fd8b053766da54489bd4a4da38274deed7
unknown <falahdelli5@gmail.com>
2026-08-15 15:40:40 +0530

    3

--- src/components/ConfigureAmplify.tsx
+++ src/components/ConfigureAmplify.tsx
@@ -1,12 +1,12 @@
-// "use client";
+"use client";
 
-// import { Amplify } from "aws-amplify";
-// import { amplifyConfig } from "@/lib/amplify-config";
-// import { useEffect } from "react";
+import { Amplify } from "aws-amplify";
+import { amplifyConfig } from "@/lib/amplify-config";
+import { useEffect } from "react";
 
-// export function ConfigureAmplifyClientSide() {
-//   useEffect(() => {
-//     Amplify.configure(amplifyConfig, { ssr: true });
-//   }, []);
-//   return null;
-// }
+export function ConfigureAmplifyClientSide() {
+  useEffect(() => {
+    Amplify.configure(amplifyConfig, { ssr: true });
+  }, []);
+  return null;
+}
